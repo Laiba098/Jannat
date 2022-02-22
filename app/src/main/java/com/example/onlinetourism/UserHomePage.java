ae417822e611bf14c8669be5e4f8b0033d656587
Laiba Abbasi <laibaabbasi098@gmail.com>
2022-02-22 08:09:28 -0700

    Online Tourism

--- app/src/main/java/com/example/onlinetourism/UserHomePage.java
+++ app/src/main/java/com/example/onlinetourism/UserHomePage.java
@@ -3,6 +3,7 @@ package com.example.onlinetourism;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class UserHomePage extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class UserHomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home_page);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.menu2, menu);
+        super.onCreateOptionsMenu(menu);
+        return true;
     }
 }
