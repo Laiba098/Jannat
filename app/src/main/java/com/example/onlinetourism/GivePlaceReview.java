f994fb05c1222c0f246324ed188360216240d512
Laiba Abbasi <laibaabbasi098@gmail.com>
2022-02-22 09:54:06 -0700

    Online Tourism

---
+++ app/src/main/java/com/example/onlinetourism/GivePlaceReview.java
@@ -0,0 +1,14 @@
+package com.example.onlinetourism;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GivePlaceReview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_give_place_review);
+    }
+}
