5884dcf62fde161902739d28617eae7345e595dd
SukhmandeepSingh774 <khehra9191@gmail.com>
2022-04-06 01:28:12 +0500

    Online Tourism

--- app/src/main/java/com/example/onlinetourism/CompanyInformation.java
+++ app/src/main/java/com/example/onlinetourism/CompanyInformation.java
@@ -1,12 +1,15 @@
 package com.example.onlinetourism;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +18,7 @@ public class CompanyInformation extends AppCompatActivity {
     TextView compname,vehicle,numberofseats,place,vehiclerent,roomrent,driverrent,foorrent, totalrent;
     DatabaseHelper dbh;
     SQLiteDatabase db;
+    String companyid,contactnumb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +72,58 @@ public class CompanyInformation extends AppCompatActivity {
 
         getMenuInflater().inflate(R.menu.menu3, menu);
         super.onCreateOptionsMenu(menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+
+
+        // return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.callcompany:
+
+
+
+                String[] columns = {"CompanyId"};
+                String[] columns1 = {"Contact"};
+                Cursor cursor = db.query("TourPlans", columns, "CompanyName=?", new String[]{compname1}, null, null, null);
+
+
+                while (cursor.moveToNext()) {
+                   companyid = cursor.getString(0);
+                }
+
+                Cursor cursor1 = db.query("ServiceProvider", columns1, "ID=?", new String[]{companyid}, null, null, null);
+
+
+                while (cursor1.moveToNext()) {
+                    contactnumb = cursor1.getString(0);
+                }
+             //   Toast.makeText(CompanyInformation.this, "number is "+contactnumb, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:"+contactnumb));
+                startActivity(intent);
+
+              break;
+
+            case R.id.chat:
+              break;
+
+            case R.id.googlemap:
+                break;
+
+            case R.id.checkreview:
+                break;
+
+            case R.id.giveplacereview:
+                break;
+
+            case R.id.givecompreview:
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
         return true;
     }
 }
