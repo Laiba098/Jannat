d51d39973bf6469d3408502f5d160f15d324e618
SukhmandeepSingh774 <khehra9191@gmail.com>
2022-04-17 01:11:11 +0500

    Online Tourism

--- app/src/main/java/com/example/onlinetourism/Chat.java
+++ app/src/main/java/com/example/onlinetourism/Chat.java
@@ -1,5 +1,7 @@
 package com.example.onlinetourism;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -14,11 +16,15 @@ public class Chat extends AppCompatActivity {
     ListView listView;
     ImageView sendbtn;
     EditText messagebox;
+    DatabaseHelper dbHelper;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        dbHelper = new DatabaseHelper(this);
+        db = dbHelper.getWritableDatabase();
         Bundle bn = getIntent().getExtras();
         serviceseekername= bn.getString("serviceseekername");
         serviceprovidername= bn.getString("serviceprovidername");
@@ -30,9 +36,38 @@ public class Chat extends AppCompatActivity {
         tablecompcust=serviceprovidername+serviceseekername;
         tablecustcom=serviceseekername+serviceprovidername;
 
-        Toast.makeText(Chat.this, "Tables are:" +tablecustcom+tablecompcust, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(Chat.this, "Tables are:" +tablecustcom+tablecompcust, Toast.LENGTH_SHORT).show();
+Boolean res=isTableExists(db,tablecompcust);
+Boolean res1=isTableExists1(db,tablecustcom);
+if (res==true || res1==true)
+{
+    Toast.makeText(Chat.this, "Table exit", Toast.LENGTH_SHORT).show();
+
+}
+else
+{
+    Toast.makeText(Chat.this, "Table not exit", Toast.LENGTH_SHORT).show();
+
+}
 
 
-
+    }
+    private boolean isTableExists(SQLiteDatabase db, String table){
+        String sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='"+tablecompcust+"'";
+        Cursor mCursor = db.rawQuery(sql, null);
+        if (mCursor.getCount() > 0) {
+            return true;
+        }
+        mCursor.close();
+        return false;
+    }
+    private boolean isTableExists1(SQLiteDatabase db, String table){
+        String sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='"+tablecustcom+"'";
+        Cursor mCursor = db.rawQuery(sql, null);
+        if (mCursor.getCount() > 0) {
+            return true;
+        }
+        mCursor.close();
+        return false;
     }
 }
